435f10b1763e691a29f68dbfccbcee9bccd0021f
Laurent Marquet <laurent.marquet@laposte.net>
2024-09-26 12:03:56 +0200

    - Revival of Bundle (26/09/2024)

---
+++ src/DependencyInjection/c975LServicesExtension.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * (c) 2024: 975L <contact@975l.com>
+ * (c) 2024: Laurent Marquet <laurent.marquet@laposte.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace c975L\ShopBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+/**
+ * DI Extension Class
+ * @author Laurent Marquet <laurent.marquet@laposte.net>
+ * @copyright 2024 975L <contact@975l.com>
+ */
+class c975LShopExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+    }
+}
